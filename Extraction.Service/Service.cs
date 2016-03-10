50fa572ad603a9c6a86a0e5395c129f089732f01
Brian Mancini <bmancini@gmail.com>
2016-03-10 10:22:56 -0500

    Added extraction service and inclusions for sub-modules

---
+++ Extraction.Service/Service.cs
@@ -0,0 +1,71 @@
+﻿using Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroService.TextAndConcept
+{
+    public class Service
+    {
+        App app;
+        Extraction.Basis.Runner conceptExtractor;
+        Extraction.DtSearch.Runner textExtractor;
+
+        public void Start()
+        {
+            app = new App();
+            app.Start("pgp187-bmanci01", "app", "app");
+            app.Handle("concepts.extract", HandleConceptExtract);
+        }
+
+        public void Stop()
+        {
+            app.Stop();
+        }
+
+        async Task<string> HandleConceptExtract(string file)
+        {
+            var text = await HandleTextExtract(file);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            conceptExtractor.Process(text);
+            sw.Stop();
+            //await app.Publish("log.cex", file + "|HandleConceptExtract|" + sw.ElapsedMilliseconds);
+
+            return "ok";
+        }
+
+        async Task<string> HandleTextExtract(string file)
+        {
+            var fileBytes = await ReadFileText(file);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var text = textExtractor.Process(fileBytes);
+            sw.Stop();
+            //await app.Publish("log.cex", file + "|HandleTextExtract|" + sw.ElapsedMilliseconds);
+
+            return text;
+        }
+
+        async Task<byte[]> ReadFileText(string file)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var fileInfo = new FileInfo(file);
+            var length = fileInfo.Length;
+            byte[] buffer = new byte[length];
+            using (var fs = fileInfo.Open(FileMode.Open))
+            {
+                await fs.ReadAsync(buffer, 0, (int)length);
+            }
+            sw.Stop();
+            //await app.Publish("log.cex", file + "|fileSize|" + length + "|ReadFileText|" + sw.ElapsedMilliseconds);
+
+            return buffer;
+        }
+    }
+}
